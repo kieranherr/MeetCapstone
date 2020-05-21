b99e320c69d9c0ee9f60d66b082aca59e67a11c7
Kieran Herr <kieranherr@gmail.com>
2020-05-21 14:02:34 -0500

    Init

---
+++ MeetCapstone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MeetCapstone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
